2f9a95dc6407f2af257a62dfdfb2e05c552f4dd7
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-05-22 13:44:08 +0200

    added drawing function of characters

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,9 @@
 class MovableObject{
     x = 120;
-    y = 400;
+    y = 300;
     img;
+    height = 150;
+    width = 100;
 
     loadImg(path){
         this.img = new Image(); // this.img = document.getElementById('image') --> <img>
