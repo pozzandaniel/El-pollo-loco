66cec7667b1690c6cc4734e36bbb73810b746a55
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-05-22 10:05:26 +0200

    first commit

---
+++ js/game.js
@@ -0,0 +1,13 @@
+let canvas;
+let character = new movableObject();
+let ctx;
+
+
+function init(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log(character);
+    
+    
+}
