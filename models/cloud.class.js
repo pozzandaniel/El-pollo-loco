4e0674f41e905962d8b051ed4a51448213302d68
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-05-23 20:10:28 +0200

    added movement of clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,18 @@
 class Clouds extends MovableObject{
     y = 20;
-    x = Math.random() * 200;
     height = 200;
     constructor(){
         super().loadImg('img/5.Fondo/Capas/4.nubes/1.png')
         this.width = 600;
-
+        this.x = Math.random() * 200;
+        this.moveLeft();
+        
     }
+    
+    moveLeft(){
+        setInterval(() => {
+           this.x -= 0.2;
+        }, 1000/60);
+    }
+    
 }
