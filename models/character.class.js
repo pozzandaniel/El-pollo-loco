bdf260c1c88e1a285e2321308905a3c02aea6c76
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-05-24 22:20:16 +0200

    added animation character in loop

--- models/character.class.js
+++ models/character.class.js
@@ -2,8 +2,29 @@ class Character extends MovableObject{
     height = 220;
     width = 150;
     y = 210;
+    IMAGES_WALKING = [
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-22.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-23.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-24.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-25.png',
+        'img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-26.png'
+    ];
+    currentImage = 0;
+
     constructor(){
         super().loadImg('../img/2.Secuencias_Personaje-Pepe-corrección/2.Secuencia_caminata/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 1000);
     }
 
     jump() {
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,19 @@ class MovableObject{
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadImg(path){
         this.img = new Image(); // this.img = document.getElementById('image') --> new Image() = <img>
         this.img.src = path;    //document.getElementById('image').src --> `<img src="${path}">`;
+    }
+
+    loadImages(arr){
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
     }
 
     moveRight() {
