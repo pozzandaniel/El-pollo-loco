81197341609b2f525b6acbacaff5cd27578e6c69
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-05-30 15:47:29 +0200

    added bottles rotation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,12 +1,22 @@
 class ThrowableObject extends MovableObject {
     width = 60;
     height = 100;
+
+    BOTTLE_ROTATION = [
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 3.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 4.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 5.png',
+        'img/6.botella/Rotación/Mesa de trabajo 1 copia 6.png'
+    ];
     
     constructor(x, y){
         super().loadImg('img/6.botella/1.Marcador.png');
+        this.loadImages(this.BOTTLE_ROTATION);
         this.x = x;
         this.y = y;
         this.throw();
+        this.rotation();
+       
         
         
        
@@ -18,5 +28,11 @@ class ThrowableObject extends MovableObject {
         setInterval(() => {
             this.x += 15;
         }, 25);
+    }
+
+    rotation(){
+        setInterval(() => {
+            this.playAnimation(this.BOTTLE_ROTATION);
+        }, 100);
     }
 }
