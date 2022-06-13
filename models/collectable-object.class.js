24cf2a822f0f7021fca5a8c2c5051238ef5560c3
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-06-13 10:24:21 +0200

    collectable-object.class.js commented

--- models/collectable-object.class.js
+++ models/collectable-object.class.js
@@ -1,7 +1,5 @@
 class CollectableObject extends MovableObject {
     img;
-    
-    
     aboveGround = 360;
     bottle1 = 'img/6.botella/2.Botella_enterrada1.png';
     bottle2 = 'img/6.botella/2.Botella_enterrada2.png';
@@ -18,7 +16,6 @@ class CollectableObject extends MovableObject {
         './img/8.Coin/Moneda2.png',
     ];
    
-
     constructor(path){
         super().loadImg(path);
         this.loadImages(this.BOTTLE_IMAGES);
@@ -28,6 +25,11 @@ class CollectableObject extends MovableObject {
         this.animate(path);
     }
     
+    /**
+     * This function attributes the collectable object a place in the canvas.
+     * The collectable object can be a bottle or a coin.
+     * @param {String} path - It describes the path that portray an image.
+     */
     givePlace(path){
         this.x = 100+ Math.random()* 719*6;
         if(path == this.bottle1 || path == this.bottle2){
@@ -36,7 +38,12 @@ class CollectableObject extends MovableObject {
             this.y = 80+ Math.random() * this.aboveGround -80;
         }
     }
-
+    /**
+     * If the collectable object is a coin, a different size is attributed according to the different image.
+     * 
+     * @param {string} path - The path or source of the image is used to determine different sizes.
+     * The dafault size is 70px for width and height.
+     */
     setSize(path){
         if(path == this.coin1){
             this.height = 110;
@@ -48,9 +55,12 @@ class CollectableObject extends MovableObject {
             this.height = 70;
             this.width = 70;
         }
-
     }
-
+    /**
+     * This function animates the collectable objects drawn on the canvas.
+     * This animation occur regularly every 500 milliseconds.
+     * @param {string} path - the source of the image, it is used to identify the nature of the collectable object.
+     */
     animate(path){
         if(path == this.bottle1 || path == this.bottle2){
             setInterval(()=>{
